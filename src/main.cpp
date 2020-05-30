7143dfdefb9cc8d2dd9e7f8641a106c7a6bf921b
Andrew Lim <2000andrewlim@gmail.com>
2020-05-30 14:22:04 -0700

    add zero function

--- src/main.cpp
+++ src/main.cpp
@@ -3,15 +3,19 @@
 const int switchPin = 7;
 const int motorPin = 9;
 
-void setup() {
-  pinMode(switchPin, INPUT);
-  pinMode(motorPin, OUTPUT);
-  Serial.begin(9600);
+void zeroMotor() {
   // motor1.drive(slowSpeed);
   digitalWrite(motorPin, HIGH);
   while (digitalRead(switchPin));
   // motor1.brake();
   digitalWrite(motorPin, LOW);
+}
+
+void setup() {
+  pinMode(switchPin, INPUT);
+  pinMode(motorPin, OUTPUT);
+  Serial.begin(9600);
+  zeroMotor();
 }
 
 void loop() {
