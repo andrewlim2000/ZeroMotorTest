10cafab3dfd72796b6b5ab18f2670288c0c8c27b
Andrew Lim <2000andrewlim@gmail.com>
2020-05-30 13:47:58 -0700

    refactor

--- src/main.cpp
+++ src/main.cpp
@@ -2,24 +2,14 @@
 
 const int switchPin = 7;
 const int motorPin = 9;
-int switchState = 0;
 
 void setup() {
   pinMode(switchPin, INPUT);
   pinMode(motorPin, OUTPUT);
   Serial.begin(9600);
-  switchState = digitalRead(switchPin);
-  bool zero = false;
-  while (!zero) {
-    // motor1.drive(slowSpeed);
-    digitalWrite(motorPin, HIGH);
-    switchState = digitalRead(switchPin);
-    if (switchState == LOW) {
-      // motor1.brake();
-      digitalWrite(motorPin, LOW);
-      zero = true;
-    }
-  }
+  digitalWrite(motorPin, HIGH);
+  while (digitalRead(switchPin));
+  digitalWrite(motorPin, LOW);
 }
 
 void loop() {
